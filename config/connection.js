3c32eb3cf9fc14198d67ebdbe389413dc36fbd9f
nellie-geek <janellegieck@msn.com>
2019-04-12 16:26:51 -0600

    Added JAWSdb.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+var connection = mysql.createConnection("mysql://r313lxi9y4ek45xp:nmerzqyz9rbgoxfz@gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mr9ippr1m29aiuce");
 
 // Make connection.
 connection.connect(function(err) {
@@ -20,3 +14,12 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
+
+// {
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// }
